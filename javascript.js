03c931679dfa6d0f01d30ba19e70c0319a02c40a
Leyla Carmona <leylacarmona2010@hotmail.es>
2025-06-01 14:17:43 -0600

    clase 14

---
+++ javascript.js
@@ -0,0 +1,10 @@
+formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", actionformulario);
+
+function actionformulario(event){
+    event.preventDefault();
+    alert("Se contactarán con usted pronto");
+    console.log("Estoy en mi function actionformulario");
+    formulario.reset();
+}
